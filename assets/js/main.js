640f083ac755ebdc64157c4bf38d3ea25d143f29
LeidyRosmery <rosm3rita@hotmail.com>
2017-05-18 15:32:13 -0500

    validacion formulario

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+
+var nombre=document.getElementById("first_name");
+var apellido=document.getElementById("last_name");
+function typeOracion(){
+  if(this.getAttribute("type")=="text"){
+     var arrDato=this.value.split(" ");
+     var datoConMayuscula="";
+     for (var i = 0; i < arrDato.length; i++) {
+       datoConMayuscula+=arrDato[i].charAt(0).toUpperCase()+ arrDato[i].substring(1).toLowerCase() + " " ;
+     }
+     this.value=datoConMayuscula;
+   }
+}
+
+var validaLetras=function(e){
+  var codigoTecla=e.keyCode;
+  if ((codigoTecla>=97 && codigoTecla <=122)|| (codigoTecla>=65 && codigoTecla <= 90) || codigoTecla==32 || codigoTecla==39) {
+return true;
+  }else {
+    return false;
+  }
+};
+nombre.onkeypress=validaLetras;
+apellido.onkeypress=validaLetras;
+nombre.onblur=typeOracion;
+apellido.onblur=typeOracion;
