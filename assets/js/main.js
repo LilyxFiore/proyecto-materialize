abaa8ce6058ed88960c2d764cedab4435b0c7ffa
EmmAlva <clioemma_28@hotmail.com>
2017-05-18 18:11:19 -0500

    Modal coincidir imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,22 @@ $(document).ready(function(){
     $('.modal').modal();
   });
 
+/*MODAL-IMAGEN*/
+var imgPortafolio = document.getElementsByClassName('imagen-portafolio');
+var imgmodal = document.getElementById("img1");
+
+for(var i = 0; i<imgPortafolio.length; i++){
+	
+	imgPortafolio[i].addEventListener('click',function(){
+
+		var ruta = this.getAttribute("src");
+		img1.src = ruta;
+
+	})
+
+
+	
+
+
+}
+console.log(imgPortafolio[1].src);
