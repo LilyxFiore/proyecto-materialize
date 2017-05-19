a06f7f237219fdf9d126b53f20b8aa881a51f74a
EmmAlva <clioemma_28@hotmail.com>
2017-05-18 19:07:49 -0500

    fixed imag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+  });*/
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
   });
+        
+
+
 
 /*MODAL-IMAGEN*/
 var imgPortafolio = document.getElementsByClassName('imagen-portafolio');
@@ -10,14 +17,10 @@ var imgmodal = document.getElementById("img1");
 for(var i = 0; i<imgPortafolio.length; i++){
 	
 	imgPortafolio[i].addEventListener('click',function(){
-
 		var ruta = this.getAttribute("src");
 		img1.src = ruta;
 
 	})
-
-
-	
 
 
 }
