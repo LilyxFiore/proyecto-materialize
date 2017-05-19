131a709b134c2fe55ddeeeac13ce011f9a85411e
EmmAlva <clioemma_28@hotmail.com>
2017-05-18 23:44:28 -0500

    Modal ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-/*$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });*/
-
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.materialboxed').materialbox();
   });
-        
-
-
-
-/*MODAL-IMAGEN*/
-var imgPortafolio = document.getElementsByClassName('imagen-portafolio');
-var imgmodal = document.getElementById("img1");
-
-for(var i = 0; i<imgPortafolio.length; i++){
-	
-	imgPortafolio[i].addEventListener('click',function(){
-		var ruta = this.getAttribute("src");
-		img1.src = ruta;
-
-	})
-
-
-}
-console.log(imgPortafolio[1].src);
